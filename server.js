7809981b20a59b4eb1db70420482cd8afef748e7
Kehinde Adelugba <kennyadenat09@gmail.com>
2019-04-29 15:41:26 +0100

    42920191541

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist/yinkweb'));
 
-app.listen(4400 || 8000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started on port`);
 });
 
